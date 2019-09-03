43b659059d28702947f71b629304d0291b64a02b
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-09-03 16:00:22 +0100

    chore: package versions

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    const arr = [
-        "first",
-        "second",
-        "third",
-        "fourth",
-        "fifth",
-        "sixth",
-        "seventh",
-    ];
     return (
         <div className="App">
             <header className="App-header">Hello world!</header>
