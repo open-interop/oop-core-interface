2a66bd7356760cb84cbf8d65652f297d5bc6096d
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-09-22 15:43:08 +0100

    feat: showing as active in side nav even with param in path

--- src/components/View/SideNavigation.js
+++ src/components/View/SideNavigation.js
@@ -3,25 +3,41 @@ import { Navigation } from "baseui/side-navigation";
 import logo from "../../resources/open_interop_logo_square.png";
 
 const SideNavigation = props => {
+    const items = [
+        {
+            title: "Home",
+            itemId: "/",
+        },
+        {
+            title: "Devices",
+            itemId: "/devices",
+        },
+        {
+            title: "Settings",
+            itemId: "/settings",
+        },
+    ];
+
+    const getActiveItem = pathName => {
+        if (pathName === "/") {
+            return pathName;
+        }
+
+        const activeItem = items
+            .filter(item => item.itemId !== "/")
+            .find(item =>
+                props.history.location.pathname.includes(item.itemId),
+            );
+
+        return activeItem ? activeItem.itemId : "/";
+    };
+
     return (
         <>
             <img src={logo} alt="logo" />
             <Navigation
-                items={[
-                    {
-                        title: "Home",
-                        itemId: "/",
-                    },
-                    {
-                        title: "Devices",
-                        itemId: "/devices",
-                    },
-                    {
-                        title: "Settings",
-                        itemId: "/settings",
-                    },
-                ]}
-                activeItemId={props.history.location.pathname}
+                items={items}
+                activeItemId={getActiveItem(props.history.location.pathname)}
                 onChange={({ event, item }) => {
                     event.preventDefault();
                     props.history.push(item.itemId);
