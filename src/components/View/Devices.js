872e1d3ebcf5904562154ec7a4ac825bf76f00c9
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-09-22 15:41:34 +0100

    feat: showing error message/loading when fetching devices

--- src/components/View/Devices.js
+++ src/components/View/Devices.js
@@ -5,28 +5,39 @@ import OopCore from "../../OopCore";
 
 const Devices = () => {
     const [devices, setDevices] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
-        OopCore.getDevices().then(response => setDevices(response));
+        OopCore.getDevices()
+            .then(response => setDevices(response))
+            .catch(error => setErrorMessage(error.message));
     }, []);
 
-    return (
-        <>
-            <SortableTable
-                data={devices}
-                mapFunction={(columnName, content) => {
-                    if (columnName === "id") {
-                        return (
-                            <Link to={`/devices/${content}/transmissions`}>
-                                {content}
-                            </Link>
-                        );
-                    }
-                }}
-                columns={[{ id: "id", name: "Device ID" }]}
-            />
-        </>
-    );
+    if (errorMessage) {
+        return <div>{errorMessage}</div>;
+    }
+
+    if (devices && devices.length) {
+        return (
+            <>
+                <SortableTable
+                    data={devices}
+                    mapFunction={(columnName, content) => {
+                        if (columnName === "id") {
+                            return (
+                                <Link to={`/devices/${content}/transmissions`}>
+                                    {content}
+                                </Link>
+                            );
+                        }
+                    }}
+                    columns={[{ id: "id", name: "Device ID" }]}
+                />
+            </>
+        );
+    }
+
+    return <div>Loading devices...</div>;
 };
 
 export { Devices };
