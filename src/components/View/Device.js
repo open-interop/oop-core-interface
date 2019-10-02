e62bc374b92a7a479db69e6455991c14d19aa357
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-10-02 17:13:32 +0100

    feat: showing device details

--- src/components/View/Device.js
+++ src/components/View/Device.js
@@ -1,7 +1,49 @@
-import React from "react";
+/* eslint-disable camelcase */
+import React, { useState } from "react";
+import { DataProvider } from "../Universal";
+import { Form } from "../Global";
+import OopCore from "../../OopCore";
 
 const Device = props => {
-    return <div>device page</div>;
+    const [device, setDevice] = useState({});
+
+    const {
+        authentication_headers,
+        authentication_query,
+        ...formData
+    } = device;
+
+    return (
+        <div className="content-wrapper">
+            <DataProvider
+                getData={() => {
+                    return OopCore.getDevice(props.match.params.deviceId).then(
+                        response => {
+                            setDevice(response);
+                            return response;
+                        },
+                    );
+                }}
+                renderData={() => (
+                    <>
+                        <Form
+                            readOnly={true}
+                            data={formData}
+                            setData={setDevice}
+                            dataLabels={
+                                new Map([
+                                    ["site_id", "Site"],
+                                    ["device_group_id", "Group"],
+                                    ["name", "Name"],
+                                    ["active", "Active"],
+                                ])
+                            }
+                        />
+                    </>
+                )}
+            />
+        </div>
+    );
 };
 
 export { Device };
