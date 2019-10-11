0c71494161bde52d4ef83d151489e1ac5e9f1881
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-10-11 14:40:13 +0100

    feat: device tempr page (WIP)

--- src/components/View/DeviceTempr.js
+++ src/components/View/DeviceTempr.js
@@ -1,7 +1,215 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "baseui/button";
+import { FormControl } from "baseui/form-control";
+import { Input } from "baseui/input";
+import { Select } from "baseui/select";
+import { Checkbox, STYLE_TYPE } from "baseui/checkbox";
+import ArrowLeft from "baseui/icon/arrow-left";
+import { DataProvider } from "../Universal";
+import OopCore from "../../OopCore";
 
 const DeviceTempr = props => {
-    return <div className="content-wrapper">Device Tempr page</div>;
+    const [deviceTempr, setDeviceTempr] = useState({});
+    const [updatedDeviceTempr, setUpdatedDeviceTempr] = useState({});
+    const [devices, setDevices] = useState([]);
+    const [temprs, setTemprs] = useState([]);
+
+    const blankDeviceTempr = props.match.params.deviceTemprId === "new";
+
+    const getDeviceTempr = () => {
+        return blankDeviceTempr
+            ? Promise.resolve({
+                  deviceId: null,
+                  temprId: null,
+                  template: {},
+              })
+            : OopCore.getDeviceTempr(
+                  props.match.params.deviceGroupId,
+                  props.match.params.deviceTemprId,
+              );
+    };
+
+    const getData = () => {
+        return Promise.all([
+            getDeviceTempr(),
+            OopCore.getDevices(),
+            OopCore.getTemprs(props.match.params.deviceGroupId),
+        ]).then(([tempr, devices, temprs]) => {
+            console.log(tempr);
+            const devicesForThisGroup = devices.data.filter(
+                device =>
+                    device.deviceGroupId ===
+                    Number(props.match.params.deviceGroupId),
+            );
+            setDevices(devicesForThisGroup);
+            setTemprs(temprs.data);
+            setDeviceTempr(tempr);
+            setUpdatedDeviceTempr(tempr);
+            return tempr;
+        });
+    };
+
+    const refreshDeviceTempr = tempr => {
+        setDeviceTempr(tempr);
+        setUpdatedDeviceTempr(tempr);
+    };
+
+    const allDeviceTemprsPath = props.location.pathname.substr(
+        0,
+        props.location.pathname.lastIndexOf("/"),
+    );
+
+    const setValue = (key, value) => {
+        const updatedData = { ...updatedDeviceTempr };
+        updatedData[key] = value;
+        setUpdatedDeviceTempr(updatedData);
+    };
+
+    const renderTemplate = template => {
+        return <div> this is the template</div>;
+    };
+
+    return (
+        <div className="content-wrapper">
+            <Button $as={Link} to={allDeviceTemprsPath}>
+                <ArrowLeft size={24} />
+            </Button>
+            <h2>{blankDeviceTempr ? "Create Tempr" : "Edit Tempr"}</h2>
+            <DataProvider
+                getData={() => {
+                    return getData();
+                }}
+                renderData={() => (
+                    <>
+                        <FormControl
+                            label="Device"
+                            key={"form-control-group-device"}
+                        >
+                            <Select
+                                options={devices}
+                                labelKey="name"
+                                valueKey="id"
+                                searchable={false}
+                                onChange={event => {
+                                    setValue("deviceId", event.value[0].id);
+                                }}
+                                value={devices.find(
+                                    item =>
+                                        item.id === updatedDeviceTempr.deviceId,
+                                )}
+                            />
+                        </FormControl>
+                        <FormControl
+                            label="Tempr"
+                            key={"form-control-group-tempr"}
+                        >
+                            <Select
+                                options={temprs}
+                                labelKey="name"
+                                valueKey="id"
+                                searchable={false}
+                                onChange={event => {
+                                    setValue("temprId", event.value[0].id);
+                                }}
+                                value={devices.find(
+                                    item =>
+                                        item.id === updatedDeviceTempr.temprId,
+                                )}
+                            />
+                        </FormControl>
+                        <FormControl
+                            label="Endpoint type"
+                            key={"form-control-group-endpoint-type"}
+                        >
+                            <Input
+                                id={"input-endpoint-type"}
+                                value={
+                                    updatedDeviceTempr.endpointType || "http"
+                                }
+                                disabled
+                            />
+                        </FormControl>
+                        <FormControl
+                            label="Queue response"
+                            key={`form-control-queue-response`}
+                        >
+                            <Checkbox
+                                checked={updatedDeviceTempr.queueResponse}
+                                onChange={() =>
+                                    setValue(
+                                        "queueResponse",
+                                        !updatedDeviceTempr.queueResponse,
+                                    )
+                                }
+                                checkmarkType={STYLE_TYPE.toggle}
+                            />
+                        </FormControl>
+                        <FormControl
+                            label="Last updated"
+                            key={`form-control-queue-updated-at`}
+                        >
+                            <Input
+                                type="datetime-local"
+                                id={"input-updated"}
+                                value={updatedDeviceTempr.updatedAt}
+                                disabled
+                            />
+                        </FormControl>
+                        <FormControl
+                            label="Template"
+                            key={"form-control-group-body-template"}
+                        >
+                            {renderTemplate(updatedDeviceTempr.template)}
+                        </FormControl>
+                        <Button
+                            onClick={() => {
+                                if (blankDeviceTempr) {
+                                    return OopCore.createDeviceTempr(
+                                        props.match.params.deviceGroupId,
+                                        updatedDeviceTempr,
+                                    )
+                                        .then(response => {
+                                            refreshDeviceTempr(response);
+                                            props.history.replace(
+                                                `${allDeviceTemprsPath}/${response.id}`,
+                                            );
+                                        })
+                                        .catch(error => {
+                                            console.error(error);
+                                        });
+                                } else {
+                                    OopCore.updateDeviceTempr(
+                                        props.match.params.deviceGroupId,
+                                        props.match.params.temprId,
+                                        updatedDeviceTempr,
+                                    )
+                                        .then(response =>
+                                            refreshDeviceTempr(response),
+                                        )
+                                        .catch(error => {
+                                            console.error(error);
+                                        });
+                                }
+                            }}
+                            disabled={
+                                blankDeviceTempr
+                                    ? false
+                                    : Object.keys(deviceTempr).every(
+                                          key =>
+                                              deviceTempr[key] ===
+                                              updatedDeviceTempr[key],
+                                      )
+                            }
+                        >
+                            {blankDeviceTempr ? "Create" : "Save"}
+                        </Button>
+                        {props.error && <div>{props.error}</div>}
+                    </>
+                )}
+            />
+        </div>
+    );
 };
 
 export { DeviceTempr };
