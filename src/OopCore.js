d6ab007aed189a7a8f2626392a2c346add00982f
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-10-15 12:47:54 +0100

    feat: some error handling

--- src/OopCore.js
+++ src/OopCore.js
@@ -214,24 +214,19 @@ class OopCore extends EventEmitter {
     }
 
     createDeviceTemprObject = data => {
-        data.device_id = data.deviceId;
-        delete data.deviceId;
-        data.tempr_id = data.temprId;
-        delete data.temprId;
-        data.endpoint_type = data.endpointType;
-        delete data.endpointType;
-        data.queue_response = data.queueResponse;
-        delete data.queueResponse;
-        data.options = data.template;
-        delete data.template;
-        delete data.createdAt;
-        delete data.updatedAt;
-        delete data.id;
-        return data;
+        const result = {};
+        result.device_id = data.deviceId;
+        result.tempr_id = data.temprId;
+        result.endpoint_type = data.endpointType;
+        result.queue_response = data.queueResponse;
+        result.options = { ...data.template };
+        return result;
     };
 
     updateDeviceTempr(deviceGroupId, deviceTemprId, data) {
-        const payload = { device_tempr: this.createDeviceTemprObject(data) };
+        const payload = {
+            device_tempr: this.createDeviceTemprObject(data),
+        };
         return this.makeRequest(
             `/device_groups/${deviceGroupId}/device_temprs/${deviceTemprId}`,
             RequestType.PUT,
@@ -240,7 +235,9 @@ class OopCore extends EventEmitter {
     }
 
     createDeviceTempr(deviceGroupId, data) {
-        const payload = { device_tempr: this.createDeviceTemprObject(data) };
+        const payload = {
+            device_tempr: this.createDeviceTemprObject(data),
+        };
         return this.makeRequest(
             `/device_groups/${deviceGroupId}/device_temprs`,
             RequestType.POST,
