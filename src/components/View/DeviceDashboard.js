9a1032b01e5bfb0983440c10fb084a09de5a8f62
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-11-11 12:36:42 +0000

    feat: last transmissions table

--- src/components/View/DeviceDashboard.js
+++ src/components/View/DeviceDashboard.js
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, KIND } from "baseui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faEdit } from "@fortawesome/free-solid-svg-icons";
-import { DataProvider } from "../Universal";
+import {
+    faChevronLeft,
+    faEdit,
+    faCheck,
+    faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { DataProvider, Table } from "../Universal";
 import { ListItem, ListItemLabel } from "baseui/list";
-import { Card, StyledBody } from "baseui/card";
-import { Map, TileLayer } from "react-leaflet";
+import { Card, StyledBody, StyledAction } from "baseui/card";
+import { Map, TileLayer, Marker } from "react-leaflet";
 import OopCore from "../../OopCore";
 
 const DeviceDashboard = props => {
@@ -21,17 +26,20 @@ const DeviceDashboard = props => {
             OopCore.getDevice(props.match.params.deviceId),
             OopCore.getSites(),
             OopCore.getDeviceGroups(),
-        ]).then(([device, sites, groups]) => {
+            OopCore.getTransmissions(props.match.params.deviceId, {
+                pageSize: 5,
+            }),
+        ]).then(([device, sites, groups, transmissions]) => {
             device.site = sites.data.find(site => site.id === device.siteId);
             device.group = groups.data.find(
                 group => group.id === device.deviceGroupId,
             );
+            device.transmissions = transmissions.data;
             setDevice(device);
             return device;
         });
     };
 
-    const position = [51.505, -0.09];
     return (
         <div className="content-wrapper">
             <div className="space-between title">
@@ -59,53 +67,136 @@ const DeviceDashboard = props => {
                     return getData();
                 }}
                 renderKey={props.location.pathname}
-                renderData={() => (
-                    <>
-                        <div className="space-between">
-                            <div className="half">
-                                <Card>
-                                    <StyledBody>
-                                        <ListItem>
-                                            <div className="card-label">
-                                                <ListItemLabel description="Name">
-                                                    {device.name}
-                                                </ListItemLabel>
-                                            </div>
-                                        </ListItem>
-                                        <ListItem>
-                                            <div className="card-label">
-                                                <ListItemLabel description="Site">
-                                                    {device.site.fullName}
-                                                </ListItemLabel>
-                                            </div>
-                                        </ListItem>
-                                        <ListItem>
-                                            <div className="card-label">
-                                                <ListItemLabel description="Group">
-                                                    {device.group.name}
-                                                </ListItemLabel>
-                                            </div>
-                                        </ListItem>
-                                    </StyledBody>
-                                </Card>
+                renderData={() => {
+                    console.log(device.transmissions);
+                    return (
+                        <>
+                            <div className="space-between wrap">
+                                <div className="width-49">
+                                    <Card className="mb-20" title="Details">
+                                        <StyledBody>
+                                            <ListItem>
+                                                <div className="card-label">
+                                                    <ListItemLabel description="Name">
+                                                        {device.name}
+                                                    </ListItemLabel>
+                                                </div>
+                                            </ListItem>
+                                            <ListItem>
+                                                <div className="card-label">
+                                                    <ListItemLabel description="Site">
+                                                        {device.site.fullName}
+                                                    </ListItemLabel>
+                                                </div>
+                                            </ListItem>
+                                            <ListItem>
+                                                <div className="card-label">
+                                                    <ListItemLabel description="Group">
+                                                        {device.group.name}
+                                                    </ListItemLabel>
+                                                </div>
+                                            </ListItem>
+                                        </StyledBody>
+                                    </Card>
+                                </div>
+
+                                <div className="width-49">
+                                    {device.longitude && device.latitude ? (
+                                        <Card>
+                                            <Map
+                                                center={[
+                                                    device.longitude,
+                                                    device.latitude,
+                                                ]}
+                                                zoom={10}
+                                                className="map-component"
+                                            >
+                                                <TileLayer
+                                                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                                />
+                                                <Marker
+                                                    position={[
+                                                        device.longitude,
+                                                        device.latitude,
+                                                    ]}
+                                                ></Marker>
+                                            </Map>
+                                        </Card>
+                                    ) : null}
+                                </div>
+                                <div className="width-60">
+                                    {device.transmissions &&
+                                    device.transmissions.length ? (
+                                        <Card
+                                            title={
+                                                <div className="space-between">
+                                                    Latest Transmissions
+                                                    <Button
+                                                        kind={KIND.secondary}
+                                                        $as={Link}
+                                                        to={`/devices/${device.id}/transmissions`}
+                                                    >
+                                                        View All
+                                                    </Button>
+                                                </div>
+                                            }
+                                        >
+                                            <StyledBody>
+                                                <Table
+                                                    data={device.transmissions}
+                                                    mapFunction={(
+                                                        columnName,
+                                                        content,
+                                                    ) => {
+                                                        if (
+                                                            columnName ===
+                                                            "success"
+                                                        ) {
+                                                            return content ? (
+                                                                <FontAwesomeIcon
+                                                                    icon={
+                                                                        faCheck
+                                                                    }
+                                                                />
+                                                            ) : (
+                                                                <FontAwesomeIcon
+                                                                    icon={
+                                                                        faTimes
+                                                                    }
+                                                                />
+                                                            );
+                                                        }
+                                                        return content;
+                                                    }}
+                                                    columns={[
+                                                        {
+                                                            id:
+                                                                "transmissionUuid",
+                                                            name:
+                                                                "Transmission UUID",
+                                                        },
+                                                        {
+                                                            id: "success",
+                                                            name: "Success",
+                                                        },
+                                                        {
+                                                            id: "transmittedAt",
+                                                            name:
+                                                                "Transmitted at",
+                                                        },
+                                                    ]}
+                                                />
+                                            </StyledBody>
+                                            <StyledAction></StyledAction>
+                                        </Card>
+                                    ) : null}
+                                </div>
+                                <Card>graph card</Card>
                             </div>
-                            <div className="half">
-                                <Card className="map-component">
-                                    <Map
-                                        center={position}
-                                        zoom={10}
-                                        className="map-component"
-                                    >
-                                        <TileLayer
-                                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                        />
-                                    </Map>
-                                </Card>
-                            </div>
-                        </div>
-                    </>
-                )}
+                        </>
+                    );
+                }}
             />
         </div>
     );
