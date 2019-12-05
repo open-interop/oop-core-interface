b2ba2cc4069e70e33219c111a23d2be3cc19d4b9
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-12-05 14:31:05 +0000

    style: trying to make these circles responsive

--- src/components/Universal/DataCircle.js
+++ src/components/Universal/DataCircle.js
@@ -3,10 +3,7 @@ import React from "react";
 const DataCircle = props => {
     return (
         <div className="flex-column center">
-            <div
-                className={`circle circle-200`}
-                style={{ backgroundColor: props.color }}
-            >
+            <div className={`circle`} style={{ backgroundColor: props.color }}>
                 {props.value}
             </div>
             <div className="data-subtitle">{props.subtitle}</div>
