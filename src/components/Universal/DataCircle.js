e6a8e91eaaacda17e39c8431b80897332c1724dd
Sara Graves <sara.graves@bluefrontier.co.uk>
2019-12-06 15:57:53 +0000

    feat: data circles

--- src/components/Universal/DataCircle.js
+++ src/components/Universal/DataCircle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const DataCircle = props => {
     return (
-        <div className="flex-column center">
+        <div className="flex-column center width-19 mt-10">
             <div className={`circle`} style={{ backgroundColor: props.color }}>
                 {props.value}
             </div>
