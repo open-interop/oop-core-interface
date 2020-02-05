e84385547e75da10dd6add2fd7f100d5677601e5
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-02-05 11:13:30 +0000

    feat: adding parent tempr selector

---
+++ src/components/Global/TemprSelector.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Select } from "baseui/select";
+
+import OopCore from "../../OopCore";
+
+const TemprSelector = props => {
+    const [temprs, setTemprs] = useState([]);
+
+    useEffect(() => {
+        const deviceGroupId = props.deviceGroup;
+        OopCore.getTemprs({ pageSize: -1, deviceGroupId }).then(temprs => {
+            setTemprs(temprs.data);
+        });
+    }, [props.deviceGroup]);
+
+    console.log(props.value);
+    console.log(temprs);
+
+    return (
+        <Select
+            {...props}
+            labelKey="name"
+            valueKey="id"
+            options={temprs}
+            value={temprs.filter(item => item.id === props.value)}
+        />
+    );
+};
+
+export { TemprSelector };
