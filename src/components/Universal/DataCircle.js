e2b8c2964343c3da189cbbf9ead74c7e02b3e13f
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-06-25 16:35:59 +0100

    fix: fixing dashboard styling

--- src/components/Universal/DataCircle.js
+++ src/components/Universal/DataCircle.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { useStyletron } from "baseui";
+import { styled } from "styletron-react";
+
+const Circle = styled("div", props => ({
+    backgroundColor: props.color,
+    borderRadius: "50%",
+    width: "70%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "4vw",
+    color: "white",
+    ":after": {
+        content: '""',
+        display: "block",
+        paddingBottom: "100%",
+    }
+}));
 
 const DataCircle = props => {
+    const [css, theme] = useStyletron();
+
     return (
-        <div className="flex-column center width-19 mt-10">
-            <div className={`circle`} style={{ backgroundColor: props.color }}>
+        <div
+            className={css({
+                padding: theme.sizing.scale300,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            })}
+        >
+            <Circle color={props.color}>
                 {props.value}
-            </div>
+            </Circle>
             <div className="data-subtitle">{props.subtitle}</div>
         </div>
     );
