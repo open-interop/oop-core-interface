f0d657d64082cd87c631d70b729c32676e85482f
benarmstrongbf <ben.armstrong@bluefrontier.co.uk>
2020-09-18 11:48:13 +0100

    chore: test request

--- oop_docker_setup.sh
+++ oop_docker_setup.sh
@@ -167,3 +167,5 @@ docker exec -it repo_oop-core_1 bundle exec rails db:create
 docker exec -it repo_oop-core_1 bundle exec rails db:migrate
 
 docker exec -it repo_oop-core_1 bin/rails runner "account = Account.create(name: 'Test Account', hostname: 'host.docker.internal'); user = User.create(email: 'test@example.com', password: 'Password123', password_confirmation: 'Password123', account: account, time_zone: 'UTC')"
+
+curl http://localhost:9001/api/v1/me
