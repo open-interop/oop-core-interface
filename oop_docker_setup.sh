da5d21e4dbe33407ce3e88419a26247299179633
benarmstrongbf <ben.armstrong@bluefrontier.co.uk>
2020-09-18 11:35:09 +0100

    chore: testing docker cp

--- oop_docker_setup.sh
+++ oop_docker_setup.sh
@@ -166,4 +166,4 @@ docker exec -it repo_oop-core_1 bundle exec rails db:create
 
 docker exec -it repo_oop-core_1 bundle exec rails db:migrate
 
-docker exec -it repo_oop-core_1 bin/rails open_interop:setup_initial_account
+docker exec -it repo_oop-core_1 bin/rails runner "account = Account.create(name: 'Test Account', hostname: 'host.docker.internal'); user = User.create(email: 'test@example.com', password: 'Password123', password_confirmation: 'Password123', account: account, time_zone: 'UTC')"
