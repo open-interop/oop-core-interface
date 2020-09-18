26b508b4c8b929e39f359d203bde77de88be4e93
benarmstrongbf <ben.armstrong@bluefrontier.co.uk>
2020-09-18 11:20:42 +0100

    chore: testing docker cp

--- oop_docker_setup.sh
+++ oop_docker_setup.sh
@@ -10,6 +10,8 @@ services:
       - "9001:9001"
     env_file:
       - all.env
+    depends_on:
+      - database
     volumes:
       - gem_cache:/gems
 
@@ -47,7 +49,15 @@ services:
     env_file:
       - all.env
 
+  database:
+    image: postgres
+    env_file:
+      - all.env
+    volumes:
+      - db_data:/var/lib/postgresql/data
+
 volumes:
+  db_data:
   gem_cache:
 EOF
 
@@ -56,8 +66,8 @@ NODE_ENV=production
 RAILS_ENV=production
 
 # All repos
-OOP_AMQP_ADDRESS=amqp://guest:guest@172.17.0.1
-RABBITMQ_URL=amqp://guest:guest@172.17.0.1
+OOP_AMQP_ADDRESS=amqp://guest:guest@host.docker.internal
+RABBITMQ_URL=amqp://guest:guest@host.docker.internal
 OOP_ERROR_EXCHANGE_NAME=oop.errors
 OOP_JSON_ERROR_Q=oop.errors.json
 OOP_EXCHANGE_NAME=oop
@@ -76,7 +86,7 @@ OOP_CORE_DEVICE_UPDATE_EXCHANGE=oop.core.devices
 # Tempr
 OOP_TEMPR_INPUT_Q=oop.hasauth.messages
 OOP_TEMPR_OUTPUT_Q=oop.hasauth.temprs
-OOP_CORE_API_URL=http://172.17.0.1:9001/services/v1
+OOP_CORE_API_URL=http://host.docker.internal:9001/services/v1
 
 # Scheduler
 OOP_SCHEDULER_OUTPUT_Q=oop.hasauth.messages
@@ -97,7 +107,7 @@ OOP_RECURSIVE_TEMPR_Q=oop.hasauth.temprs
 OOP_CORE_RESPONSE_Q=oop.core.transmissions
 
 # Core
-OOP_AMQP_ADDRESS=amqp://172.17.0.1
+OOP_AMQP_ADDRESS=amqp://host.docker.internal
 OOP_RENDERER_PATH=/Users/jack/Projects/OOO/oop-renderer
 
 OOP_CORE_SCHEME=http://
@@ -111,7 +121,7 @@ OOP_CORE_INTERFACE_PORT=80
 OOP_CORE_INTERFACE_PATH=/
 OOP_CORE_FROM_ADDRESS=oop@bluefrontier.co.uk
 
-DATABASE_HOST=host.docker.internal
+DATABASE_HOST=database
 POSTGRES_USER=postgres
 POSTGRES_PASSWORD=password
 POSTGRES_DB=oop_core_development
@@ -151,8 +161,6 @@ EOF
 docker-compose up -d
 
 docker cp config/. repo_oop-core_1:/app/config
-
-docker exec -it repo_oop-core_1 cat /app/config/database.yml
 
 docker exec -it repo_oop-core_1 bundle exec rails db:create
 
