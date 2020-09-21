9b25f67cfccd1d217e264166f835d0b39386dbbe
benarmstrongbf <ben.armstrong@bluefrontier.co.uk>
2020-09-21 10:42:19 +0100

    chore: install bundler to change chrome path

--- testing.rb
+++ testing.rb
@@ -2,7 +2,7 @@ require 'watir'
 require 'webdrivers'
 
 args = %w[headless disable-gpu disable-dev-shm-usage disable-software-rasterizer no-sandbox]
-browser = Watir::Browser.new :chrome, {url: "http://127.0.0.1:9515", chromeOptions: {args: args}}
+browser = Watir::Browser.new :chrome, args: args
 
 browser.goto 'localhost:3001/login'
 
