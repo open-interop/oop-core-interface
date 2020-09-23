88416473f490f8eb38eb99eb915728803bae4c69
benarmstrongbf <ben.armstrong@bluefrontier.co.uk>
2020-09-23 15:07:39 +0100

    chore: ports

--- core_setup.sh
+++ core_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec -it oop-core bundle exec rails db:create
+bundle exec rails db:create
 
-docker exec -it oop-core bundle exec rails db:migrate
+bundle exec rails db:migrate
 
-docker exec -it oop-core bin/rails runner "account = Account.create(name: 'Test Account', hostname: 'host.docker.internal'); user = User.create(email: 'test@example.com', password: 'Password123', password_confirmation: 'Password123', account: account, time_zone: 'UTC')"
+bin/rails runner "account = Account.create(name: 'Test Account', hostname: 'host.docker.internal'); user = User.create(email: 'test@example.com', password: 'Password123', password_confirmation: 'Password123', account: account, time_zone: 'UTC')"
