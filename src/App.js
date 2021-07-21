fec548bb796558fa9c9ca53cf97e870c4c65dde3
Jack Regnart <Jack.Regnart@bluefrontier.co.uk>
2021-07-21 16:05:02 +0100

    fix: fix lint issues

--- src/App.js
+++ src/App.js
@@ -1,61 +1,72 @@
 import React, { Component, Suspense, lazy } from "react";
 
 import { BaseProvider } from "baseui";
-import { Block } from 'baseui/block';
+import { Block } from "baseui/block";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 import oopTheme from "./theme";
 
-import { BrowserRouter, Route, Redirect, withRouter, Switch } from "react-router-dom";
+import {
+    BrowserRouter,
+    Route,
+    Redirect,
+    withRouter,
+    Switch,
+} from "react-router-dom";
+
 import { createBrowserHistory } from "history";
 
 import { GifSpinner } from "./components/Universal";
 import OopCore from "./OopCore";
 import "./styles/App.scss";
 import { QueryParamProvider } from "use-query-params";
-import { 
+import {
     MobileNavigation,
     MobileHeader,
     SideNavigation,
-    Header     
+    Header,
 } from "./components/View";
 
 const engine = new Styletron();
 const queryString = require("query-string");
 
-const Account = lazy(() => import('./components/View/Account'));
-const AuditLog = lazy(() => import('./components/View/AuditLog'));
-const AuditLogs = lazy(() => import('./components/View/AuditLogs'));
-const BlacklistEntries = lazy(() => import('./components/View/BlacklistEntries'));
-const BlacklistEntry = lazy(() => import('./components/View/BlacklistEntry'));
-const Dashboard = lazy(() => import('./components/View/Dashboard'));
-const Device = lazy(() => import('./components/View/Device'));
-const DeviceDashboard = lazy(() => import('./components/View/DeviceDashboard'));
-const DeviceGroup = lazy(() => import('./components/View/DeviceGroup'));
-const DeviceGroups = lazy(() => import('./components/View/DeviceGroups'));
-const Devices = lazy(() => import('./components/View/Devices'));
-const ForgotPassword = lazy(() => import('./components/View/ForgotPassword'));
-const GlobalHistory = lazy(() => import('./components/View/GlobalHistory'));
-const Layer = lazy(() => import('./components/View/Layer'));
-const Layers = lazy(() => import('./components/View/Layers'));
-const Login = lazy(() => import('./components/View/Login'));
-const Message = lazy(() => import('./components/View/Message'));
-const Messages = lazy(() => import('./components/View/Messages'));
-const PageNotFound = lazy(() => import('./components/View/PageNotFound'));
-const ResetPassword = lazy(() => import('./components/View/ResetPassword'));
-const Schedule = lazy(() => import('./components/View/Schedule'));
-const ScheduleDashboard = lazy(() => import('./components/View/ScheduleDashboard'));
-const Schedules = lazy(() => import('./components/View/Schedules'));
-const Site = lazy(() => import('./components/View/Site'));
-const Sites = lazy(() => import('./components/View/Sites'));
-const Tempr = lazy(() => import('./components/View/Tempr'));
-const TemprMap = lazy(() => import('./components/View/TemprMap'));
-const Temprs = lazy(() => import('./components/View/Temprs'));
-const Transmission = lazy(() => import('./components/View/Transmission'));
-const Transmissions = lazy(() => import('./components/View/Transmissions'));
-const User = lazy(() => import('./components/View/User'));
-const UserDashboard = lazy(() => import('./components/View/UserDashboard'));
-const Users = lazy(() => import('./components/View/Users'));
+const Account = lazy(() => import("./components/View/Account"));
+const AuditLog = lazy(() => import("./components/View/AuditLog"));
+const AuditLogs = lazy(() => import("./components/View/AuditLogs"));
+const BlacklistEntries = lazy(() =>
+    import("./components/View/BlacklistEntries"),
+);
+const BlacklistEntry = lazy(() => import("./components/View/BlacklistEntry"));
+const Dashboard = lazy(() => import("./components/View/Dashboard"));
+const Device = lazy(() => import("./components/View/Device"));
+const DeviceDashboard = lazy(() => import("./components/View/DeviceDashboard"));
+const DeviceGroup = lazy(() => import("./components/View/DeviceGroup"));
+const DeviceGroups = lazy(() => import("./components/View/DeviceGroups"));
+const Devices = lazy(() => import("./components/View/Devices"));
+const ForgotPassword = lazy(() => import("./components/View/ForgotPassword"));
+const GlobalHistory = lazy(() => import("./components/View/GlobalHistory"));
+const Layer = lazy(() => import("./components/View/Layer"));
+const Layers = lazy(() => import("./components/View/Layers"));
+const Login = lazy(() => import("./components/View/Login"));
+const Message = lazy(() => import("./components/View/Message"));
+const Messages = lazy(() => import("./components/View/Messages"));
+const PageNotFound = lazy(() => import("./components/View/PageNotFound"));
+const ResetPassword = lazy(() => import("./components/View/ResetPassword"));
+const Schedule = lazy(() => import("./components/View/Schedule"));
+const ScheduleDashboard = lazy(() =>
+    import("./components/View/ScheduleDashboard"),
+);
+const Schedules = lazy(() => import("./components/View/Schedules"));
+const Site = lazy(() => import("./components/View/Site"));
+const Sites = lazy(() => import("./components/View/Sites"));
+const Tempr = lazy(() => import("./components/View/Tempr"));
+const TemprMap = lazy(() => import("./components/View/TemprMap"));
+const Temprs = lazy(() => import("./components/View/Temprs"));
+const Transmission = lazy(() => import("./components/View/Transmission"));
+const Transmissions = lazy(() => import("./components/View/Transmissions"));
+const User = lazy(() => import("./components/View/User"));
+const UserDashboard = lazy(() => import("./components/View/UserDashboard"));
+const Users = lazy(() => import("./components/View/Users"));
 
 class App extends Component {
     constructor(props) {
@@ -135,12 +146,12 @@ class App extends Component {
             </div>
         ) : (
             <>
-                <Block display={['none', 'none', 'block']}>
+                <Block display={["none", "none", "block"]}>
                     <div className="content">
                         <Component {...props} />
                     </div>
                 </Block>
-                <Block display={['block', 'block', 'none']}>
+                <Block display={["block", "block", "none"]}>
                     <div className="content-mobile">
                         <Component {...props} />
                     </div>
@@ -149,7 +160,7 @@ class App extends Component {
         );
     };
 
-    renderRoutes = () => {
+    renderRoutes() {
         const hasUser = this.state.user;
 
         return (
@@ -157,27 +168,29 @@ class App extends Component {
                 <QueryParamProvider ReactRouterRoute={Route}>
                     {hasUser && (
                         <>
-                            <Block display={['none', 'none', 'block']}>
+                            <Block display={["none", "none", "block"]}>
                                 <this.HeaderWithRouter
                                     user={this.state.user}
                                     site={this.state.site}
                                     selectSite={this.selectSite}
                                 />
-                                <this.SideNavigationWithRouter 
-                                    selectSite={this.selectSite} 
-                                    site={this.state.site} 
-                                    user={this.state.user} />
+                                <this.SideNavigationWithRouter
+                                    selectSite={this.selectSite}
+                                    site={this.state.site}
+                                    user={this.state.user}
+                                />
                             </Block>
-                            <Block display={['block', 'block', 'none']}>
+                            <Block display={["block", "block", "none"]}>
                                 <this.MobileHeaderWithRouter
                                     user={this.state.user}
                                     site={this.state.site}
                                     selectSite={this.selectSite}
                                 />
-                                <this.MobileNavigationWithRouter 
-                                    selectSite={this.selectSite} 
-                                    site={this.state.site} 
-                                    user={this.state.user} />
+                                <this.MobileNavigationWithRouter
+                                    selectSite={this.selectSite}
+                                    site={this.state.site}
+                                    user={this.state.user}
+                                />
                             </Block>
                         </>
                     )}
@@ -194,14 +207,22 @@ class App extends Component {
                                 path="/forgot-password"
                                 exact
                                 render={props =>
-                                    this.getComponent(hasUser, ForgotPassword, props)
+                                    this.getComponent(
+                                        hasUser,
+                                        ForgotPassword,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
                                 path="/reset-password"
                                 exact
                                 render={props =>
-                                    this.getComponent(hasUser, ResetPassword, props)
+                                    this.getComponent(
+                                        hasUser,
+                                        ResetPassword,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
@@ -252,28 +273,44 @@ class App extends Component {
                                 path="/transmissions"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, Transmissions, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        Transmissions,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
                                 path="/transmissions/:transmissionId"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, Transmission, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        Transmission,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
                                 path="/device-groups"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, DeviceGroups, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        DeviceGroups,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
                                 path="/device-groups/:deviceGroupId"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, DeviceGroup, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        DeviceGroup,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
@@ -294,7 +331,11 @@ class App extends Component {
                                 path="/schedules"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, Schedules, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        Schedules,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
@@ -335,14 +376,22 @@ class App extends Component {
                                 path="/blacklist-entries"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, BlacklistEntries, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        BlacklistEntries,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
                                 path="/blacklist-entries/:blacklistEntryId"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, BlacklistEntry, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        BlacklistEntry,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
@@ -385,10 +434,14 @@ class App extends Component {
                                 render={props => {
                                     const user = this.state.user;
 
-                                    return this.getComponent(!hasUser, UserDashboard, {
-                                        ...props,
-                                        user,
-                                    });
+                                    return this.getComponent(
+                                        !hasUser,
+                                        UserDashboard,
+                                        {
+                                            ...props,
+                                            user,
+                                        },
+                                    );
                                 }}
                             />
                             <Route
@@ -430,14 +483,22 @@ class App extends Component {
                                 path="/global-history"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, GlobalHistory, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        GlobalHistory,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
                                 path="/:componentType/:componentId/audit-logs"
                                 exact
                                 render={props =>
-                                    this.getComponent(!hasUser, AuditLogs, props)
+                                    this.getComponent(
+                                        !hasUser,
+                                        AuditLogs,
+                                        props,
+                                    )
                                 }
                             />
                             <Route
@@ -448,8 +509,12 @@ class App extends Component {
                                 }
                             />
                             <Route
-                                render={props => 
-                                    this.getComponent(!hasUser, PageNotFound, props)
+                                render={props =>
+                                    this.getComponent(
+                                        !hasUser,
+                                        PageNotFound,
+                                        props,
+                                    )
                                 }
                             />
                         </Switch>
@@ -457,17 +522,20 @@ class App extends Component {
                 </QueryParamProvider>
             </BrowserRouter>
         );
-    };
+    }
 
     render() {
-        return <StyletronProvider value={engine}>
-            <BaseProvider theme={oopTheme}>
-                { this.state.isLoading
-                    ? <GifSpinner />
-                    : this.renderRoutes()
-                }
-            </BaseProvider>
-        </StyletronProvider>;
+        return (
+            <StyletronProvider value={engine}>
+                <BaseProvider theme={oopTheme}>
+                    {this.state.isLoading ? (
+                        <GifSpinner />
+                    ) : (
+                        this.renderRoutes()
+                    )}
+                </BaseProvider>
+            </StyletronProvider>
+        );
     }
 }
 
